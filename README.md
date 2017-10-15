1568da932cede797636295600af581ea19a60a21
NikitaLukashev <nloukachev@gmail.com>
2017-10-15 15:16:46 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+Kaggle - Two Sigma Connect: Rental Listing Inquiries
+
+My best single LightGBM and Xgboost for "Two Sigma Connect: Rental Listing Inquiries" competition.
+
+Public LB score: 
+	-LightGBM 0.5076
+	-Xgboost 0.5112
+ 
+Private LB score: 
+	-LightGBM 0.5074 (127/2500)
+	-Xgboost 0.5098 (180/2500)
+
+
+To Run:
+	-unzip all files from db directory into db directory
+	-run best_model.py to get same result.
+
+
+Used packages:
+
+Xgboost
+pyLightGBM
---
+++ features_utils.py
@@ -0,0 +1,95 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+import pandas as pd
+
+# tfidf
+def get_tfidf_vectorizer(df, field, vectorizer):
+    tfidf = vectorizer.fit_transform(df[field])
+    tfidf_cols = vectorizer.get_feature_names()
+    temp = pd.DataFrame(data=tfidf.todense(), columns=['tfidf_' + field + '_' + i for i in tfidf_cols])
+    df = pd.concat([df, temp], axis=1)
+    print(tfidf_cols)
+    return df
+
+
+# make aggregate of field by by_field on df
+def get_mean(df, field, by_field):
+    temp = df.groupby(by_field).mean()[[field]].reset_index()
+    temp.columns = [by_field, 'mean_of_' + field + '_by_' + by_field]
+    df = df.merge(temp, on=by_field, how='left')
+    return df
+
+
+def get_median(df, field, by_field):
+    temp = df.groupby(by_field).median()[[field]].reset_index()
+    temp.columns = [by_field, 'median_of_' + field + '_by_' + by_field]
+    df = df.merge(temp, on=by_field, how='left')
+    return df
+
+
+def get_max(df, field, by_field):
+    temp = df.groupby(by_field).max()[[field]].reset_index()
+    temp.columns = [by_field, 'max_of_' + field + '_by_' + by_field]
+    df = df.merge(temp, on=by_field, how='left')
+    return df
+
+
+def get_min(df, field, by_field):
+    temp = df.groupby(by_field).min()[[field]].reset_index()
+    temp.columns = [by_field, 'min_of_' + field + '_by_' + by_field]
+    df = df.merge(temp, on=by_field, how='left')
+    return df
+
+
+def get_sum(df, field, by_field):
+    temp = df.groupby(by_field).sum()[[field]].reset_index()
+    temp.columns = [by_field, 'sum_of_' + field + '_by_' + by_field]
+    df = df.merge(temp, on=by_field, how='left')
+    return df
+
+
+def get_std(df, field, by_field):
+    temp = df.groupby(by_field).std()[[field]].reset_index()
+    temp.columns = [by_field, 'std_of_' + field + '_by_' + by_field]
+    df = df.merge(temp, on=by_field, how='left')
+    return df
+
+
+def get_count(df, field, by_field):
+    df[field].fillna('xxx', inplace=True)
+    temp = df.groupby(by_field).count()[[field]].reset_index()
+    temp.columns = [by_field, 'count_of_' + field + '_by_' + by_field]
+    df = df.merge(temp, on=by_field, how='left')
+    return df
+
+
+def get_distinct_count(df, field, by_field):
+    df[field].fillna('xxx', inplace=True)
+    temp = df[[by_field, field]]
+    temp.drop_duplicates(inplace=True)
+    temp = temp.groupby(by_field).count()[[field]].reset_index()
+    temp.columns = [by_field, 'distinct_count_of_' + field + '_by_' + by_field]
+    df = df.merge(temp, on=by_field, how='left')
+    return df
+
+
+# make interaction of two field on df
+def get_interaction_product(df, field_1, field_2):
+    df[field_1 + '_*_' + field_2] = df[field_1] * df[field_2]
+    return df
+
+
+def get_interaction_quotient(df, field_1, field_2):
+    df[field_1 + '_/_' + field_2] = df[field_1] / df[field_2]
+    return df
+
+
+def get_interaction_sum(df, field_1, field_2):
+    df[field_1 + '_+_' + field_2] = df[field_1] + df[field_2]
+    return df
+
+
+def get_interaction_minus(df, field_1, field_2):
+    df[field_1 + '_-_' + field_2] = df[field_1] - df[field_2]
+    return df
